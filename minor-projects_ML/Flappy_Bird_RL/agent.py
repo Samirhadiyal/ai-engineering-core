32729dcf5ef197f00973780b788a1b7c97a3c841
Samir Hadiyal <hadiyal.samir26@gmail.com>
2026-06-17 16:53:46 +0530

    Add parameters.yaml for Flappy Bird agent configuration

--- minor-projects_ML/Flappy_Bird_RL/agent.py
+++ minor-projects_ML/Flappy_Bird_RL/agent.py
@@ -3,8 +3,10 @@ import gymnasium as gym
 from DQN import dqn
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from experience_replay import ReplayMemory
 import itertools
+import yaml
 
 # detect gpu and set gpu 
 if torch.cuda.is_available():
@@ -12,37 +14,63 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-
-def run(self, is_training=True, render = False):
-    env = gym.make("FlappyBird-v0", render_mode="human" if render else None)
-
-    num_states = env.observation_space.shape[0] # input dimensions
-    num_actions = env.action_space.n # output  dimensions
-    
-    policy_dqn = DQN(num_states, num_actions).to(device)
-    
-    
-    if is_training:
-        memory = ReplayMemory(10000)
-    
-    for episode in itertools.count():
+class Agent:
+    def __init__(self, param_set):
+        self.param_set = param_set
         
-        state, _ = env.reset()
-        episode_rewards = 0
-        
-        while not terminated:
-            action = env.action_space. sample()
-
+        with open("parameters.yaml", 'r') as f:
+            all_param_set = yaml.safe_load(f)
+            params = all_param_set[param_set]
             
-            next_state, reward, terminated, _, _ = env.step(action)
+        self.alpha = params["alpha"]
+        self.gamma = params["gamma"]
+        
+        self.epsilon_init = params["epsilon_init"]
+        self.epsilon_min = params["epsilon_min"]
+        self.epsilon_decay = params["epsilon_decay"]
+        
+        
+        self.replay_memory_size = params["replay_memory_size"]
+        self.mini_batch_size = params["mini_batch_size"]
+        
+        
+        self.network_sync_rate = params["network_sync_rate"]
+        self.reward_threshold = params["reward_threshold"]
+        
+        self.loss_fn = nn.MSELoss()
+        self.optimizer = None
+            
+    def run(self, is_training=True, render = False):
+    
+        env = gym.make("FlappyBird-v0", render_mode="human" if render else None)
 
-            if is_training:
-                memory.append((state, action, next_state, reward, terminated))
+        num_states = env.observation_space.shape[0] # input dimensions
+        num_actions = env.action_space.n # output  dimensions
+        
+        policy_dqn = DQN(num_states, num_actions).to(device)
+        
+        
+        if is_training:
+            memory = ReplayMemory(10000)
+        
+        for episode in itertools.count():
+            state, _ = env.reset()
+            episode_rewards = 0
+            terminated = False
+            
+            while not terminated:
+                action = env.action_space. sample()
 
-            # Checking if the payer is still alive
-            if terminated:
-                break
+                
+                next_state, reward, terminated, _, _ = env.step(action)
 
-    #env.close() - manually stop
+                if is_training:
+                    memory.append((state, action, next_state, reward, terminated))
 
+                state = new_state
+                episode_rewards += rewards
+            
+            print(f"episode={episode+1}, with total reward={episode_rewards}")
+        #env.close() - manually stop
+    
     
