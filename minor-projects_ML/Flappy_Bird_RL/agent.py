01cecff23e07b0249fe65901abecc81a90806579
Samir Hadiyal <hadiyal.samir26@gmail.com>
2026-06-17 15:43:38 +0530

    Refactor agent code to encapsulate environment setup and execution in a run method

--- minor-projects_ML/Flappy_Bird_RL/agent.py
+++ minor-projects_ML/Flappy_Bird_RL/agent.py
@@ -1,19 +1,37 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
+from DQN import dqn
+import torch
+import torch.nn as nn
 
-env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=True)
+# detect gpu and set gpu 
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
 
-obs, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space. sample()
 
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
+def run(self, is_training=True, render = False):
+    env = gym.make("FlappyBird-v0", render_mode="human" if render else None)
 
-    # Checking if the payer is still alive
-    if terminated:
-        break
+    num_states = env.observation_space.shape[0] # input dimensions
+    num_actions = env.action_space.n # output  dimensions
+    
+    policy_dqn = DQN(num_states, num_actions).to(device)
+    
+    
+    
+    state, _ = env.reset()
+    while True:
+        # Next action:
+        # (feed the observation to your agent here)
+        action = env.action_space. sample()
 
-env.close()
+        # Processing: terminated => done 
+        next_state, reward, terminated, _, _ = env.step(action)
+
+        # Checking if the payer is still alive
+        if terminated:
+            break
+
+    env.close()
